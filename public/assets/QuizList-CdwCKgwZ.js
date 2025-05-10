5bd5e228c86b556e49e4053251c291dca192f8c5
msisaif <msi132043@gmail.com>
2025-05-10 08:00:53 +0600

    feat: Add Vuex modules for purchase, question, quiz, transaction, user, and user quiz management
    - Implemented state management for purchases, including fetching and creating payments.
    - Added question management with CRUD operations and error handling.
    - Developed quiz management features, including fetching, adding, updating, and deleting quizzes.
    - Created transaction management for fetching transactions and total income.
    - Implemented user management with user fetching, adding, and updating functionalities.
    - Added user quiz participation and submission handling.

---
+++ public/assets/QuizList-CdwCKgwZ.js
@@ -0,0 +1,1 @@
+import{d as r}from"./index-89nqye9T.js";import{O as t,y as o}from"./vue-BP9P6hCg.js";import"./vendor-PJuFQbji.js";const p={__name:"QuizList",setup(a){return(e,s)=>(o(),t(r))}};export{p as default};
