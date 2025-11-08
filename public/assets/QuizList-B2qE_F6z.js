91d9f7105db9870e1b0ff81cf99ecf295083ea41
msisaif <msi132043@gmail.com>
2025-11-08 11:15:18 +0600

    build: fix: purchase for other allow after self purchase

---
+++ public/assets/QuizList-B2qE_F6z.js
@@ -0,0 +1,1 @@
+import{g as r}from"./index-Co5G-AfD.js";import{O as t,y as o}from"./vue-CcEtAIOs.js";import"./vendor-PJuFQbji.js";const p={__name:"QuizList",setup(a){return(e,s)=>(o(),t(r))}};export{p as default};
